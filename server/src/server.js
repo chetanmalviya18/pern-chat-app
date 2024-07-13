02f90e33b0b8542cf12dd8d3a512787c670b7e06
chetanmalviya18 <chetanmalviya.182@gmail.com>
2024-07-13 19:05:30 +0530

    configuration for deployement

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,23 @@
 import path from "path";
 import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
 import { app, server } from "./socket/socket.js";
-// import cors from "cors";
-
-const __dirname = path.resolve();
-
-// app.use(cors());
-app.use(cookieParser());
-app.use(express.json());
-
-app.use(routes);
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(routes);
+
+if (process.env.NODE_ENV !== "development") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
