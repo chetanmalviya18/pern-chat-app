8ce941bef24bb61acbee52b78535eb891ef197ec
chetanmalviya18 <chetanmalviya.182@gmail.com>
2024-07-14 10:11:21 +0530

    changes 1

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import path from "path";
+// import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,17 +7,17 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
 
-if (process.env.NODE_ENV !== "development") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV !== "development") {
+//   app.use(express.static(path.join(__dirname, "/client/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
